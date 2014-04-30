ad925d351b5cbe6b6ac908cc89520c531f621aed
nebelhorn <nebelhorn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-30 17:16:01 +0000

    bevor rename and fic menu
    git-svn-id: https://plugins.svn.wordpress.org/yournewsapp@905746 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/0.5.6/tags/0.3.4.0/tags/0.3.2.1/include/default_css.php
@@ -0,0 +1,19 @@
+<?php
+	$css = '
+	@import url("../fonts/stylesheet.css");
+body {	
+	font-size:3em;
+	line-height:1.5em;
+	font-family:"Roboto Condensed",Roboto, Helvetica, sans-serif;
+	text-align:justify;
+}	
+img {
+	width:100%;
+	margin-bottom:10px;
+}
+ul, ol{
+	margin:0 0 0 20px; 
+}
+';
+	
+?>
